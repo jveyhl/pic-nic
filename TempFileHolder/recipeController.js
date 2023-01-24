63221a3a888d12193c9fb9f0505b3a5c38a4c83a
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-01-24 16:43:53 -0500

    Didnt manage to test yet, added some files to check as a group!

---
+++ TempFileHolder/recipeController.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Recipe } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newRecipe = await Recipe.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newRecipe);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const RecipeData = await Recipe.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!Recipe) {
+      res.status(404).json({ message: 'No Recipe found with this id!' });
+      return;
+    }
+
+    res.status(200).json(RecipeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
---
+++ TempFileHolder/recipeModel.js
@@ -0,0 +1,84 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingrediants: {
+        type: DataTypes.JSON,
+        allowNull: false,
+        validate: {
+            isValidIngrediantFormat: function(value) {
+                if(!Array.isArray(value)) {
+                    throw new err ('Ingrediants need to be an array');
+                }
+                for (let i=0; i < value.length; i++) {
+                    let ingredient= value[i];
+                    if(!ingredient.name || ingredient.quantity || ingredient.measurement) {
+                        throw new err ('Ingredients need to have a name, quantity, and measurement');
+                    }
+                    if(typeof ingredient.name !== 'string') {
+                        throw new err ('Ingredient name needs to be a string');
+                    }
+                    if (isNaN(ingredient.quantity)) {
+                        throw new err ('Quantity needs to be a number');
+                    }
+                    if (typeof ingredient.measurement !== 'string') {
+                        throw new err ('Measurement needs to be a string');
+                    }
+                }
+            }
+        }
+        // This looks like the following 
+        // {name: "STRING OF NAME OF INGREDIANT", quantity: "Number describing amount of measurement", measurement: 'name of measurement'}
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    allergens: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'allergen',
+        key: 'id',
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'recipe',
+  }
+);
+
+module.exports = Recipe;
