890bb82c7ff77812c973f8133d42a3c6b6267179
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-01-31 16:55:55 -0500

    Added first set of files (might be all of them? will go over with group) for comment functionality

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Handle a GET request to the root URL and return all comments
+router.get('/', (req, res) => {
+  Comment.findAll({})
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Handle a GET request to a specific comment ID and return the corresponding comment
+router.get('/:id', (req, res) => {
+  Comment.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Handle a POST request to the root URL and create a new comment
+router.post('/', async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Handle a DELETE request to a specific comment ID and delete the corresponding comment
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: '404 Blog ID not found' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
