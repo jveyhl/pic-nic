6c310a3c37616c9d382720aeb4f649e23370efba
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-02-01 02:53:28 -0500

    Fixed description issue

--- models/Recipe.js
+++ models/Recipe.js
@@ -16,7 +16,7 @@ Recipe.init(
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     date_created: {
       type: DataTypes.DATE,
